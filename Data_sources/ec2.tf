bb02074bca068198f2ab5c5a9949e05ed419ba41
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-04 18:10:43 +0530

    adding to ec2 instance by data source

---
+++ Data_sources/ec2.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "ubuntu-linux-2004" {
+  executable_users = ["self"]
+  most_recent      = true
+  owners           = ["521045274894"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+
+resource "aws_instance" "linux-server" {
+  ami = data.aws_ami.ubuntu-linux-2004.id
+  instance_type = t2.micro
+  key_name = ajith-keys
+}
+
+
